3e68583ad49b5a171d0cbd192d0b360a8c97ff12
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-05-01 19:02:44 +0900

    feat: tRPCで疎通確認

---
+++ src/app/utils/trpc.ts
@@ -0,0 +1,31 @@
+"use client";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCNext } from "@trpc/next";
+import type { AppRouter } from "../api/trpc/_app";
+
+export const trpc = createTRPCNext<AppRouter>({
+  config(opts) {
+    return {
+      links: [
+        httpBatchLink({
+          /**
+           * If you want to use SSR, you need to use the server's full URL
+           * @link https://trpc.io/docs/ssr
+           **/
+          url: `http://localhost:3001/api/trpc`,
+
+          // You can pass any HTTP headers you wish here
+          async headers() {
+            return {
+              // authorization: getAuthCookie(),
+            };
+          },
+        }),
+      ],
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   **/
+  ssr: false,
+});
